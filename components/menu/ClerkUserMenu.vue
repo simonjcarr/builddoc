0c8a8199e806c0831f2efdaf1764c19c719bc1a1
Simon Carr <simonjcarr@gmail.com>
2024-07-11 20:34:28 +0100

    extract topnav component and clerk menu

---
+++ components/menu/ClerkUserMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <SignedIn>
+      <v-btn>
+        <UserButton after-sign-out-url="/" class="mr-5" />
+      </v-btn>
+    </SignedIn>
+    <SignedOut>
+      <v-btn prepend-icon="mdi-account-plus">
+        <SignUpButton />
+      </v-btn>
+      <v-btn prepend-icon="mdi-login" variant="text">
+        <SignInButton />
+      </v-btn>
+    </SignedOut>
+  </div>
+</template>
+
+<script setup>
+import {
+  SignedIn,
+  SignedOut,
+  UserButton,
+  SignInButton,
+  SignUpButton,
+} from "vue-clerk";
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/nav/TopNavBar.client.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <v-app-bar color="primary" prominent>
+      <v-app-bar-nav-icon
+        variant="text"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Build Doc</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <template v-if="$vuetify.display.mdAndUp">
+       <MenuClerkUserMenu />
+      </template>
+
+      <div v-if="$vuetify.display.smAndDown">
+        <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
+      </div>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      :location="$vuetify.display.mobile ? 'bottom' : undefined"
+      temporary
+    >
+      <v-list :items="items"></v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script setup>
+
+let drawer = ref(false);
+let group = ref(null);
+let items = ref([
+  {
+    title: "Foo",
+    value: "foo",
+  },
+  {
+    title: "Bar",
+    value: "bar",
+  },
+  {
+    title: "Fizz",
+    value: "fizz",
+  },
+  {
+    title: "Buzz",
+    value: "buzz",
+  },
+]);
+watch(group, (value) => {
+  drawer.value = false;
+});
+</script>
+
+<style lang="scss" scoped></style>
