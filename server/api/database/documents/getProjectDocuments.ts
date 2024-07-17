f9697d16110a8a4e231bf657e264d08167dc04cb
Simon Carr <simonjcarr@gmail.com>
2024-07-17 08:37:48 +0100

    sort order for documents and add requirements

--- server/api/database/documents/getProjectDocuments.ts
+++ server/api/database/documents/getProjectDocuments.ts
@@ -1,6 +1,6 @@
-import { eq } from 'drizzle-orm'
+import { asc, desc, eq } from 'drizzle-orm'
 import { db } from '~/server/db'
-import { projects } from '~/server/db/schema'
+import { documents, projects } from '~/server/db/schema'
 export default defineEventHandler(async (event) => {
     const eventData: { projectId: number } = getQuery(event)
 
@@ -11,8 +11,11 @@ export default defineEventHandler(async (event) => {
     const projectDocuments = await db.query.projects.findFirst({
         where: eq(projects.id, eventData.projectId),
         with: {
-            documents: true
-        }
+            documents: {
+                orderBy: asc(documents.name)
+            }
+        },
     })
+    console.log(projectDocuments)
     return projectDocuments
 })
